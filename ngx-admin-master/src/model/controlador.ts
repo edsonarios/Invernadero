9b0af9b6b0334d7daf970cf6d31f92c186a53bf6
edsonarios <edsonrios9@gmail.com>
2019-03-07 20:45:35 -0400

    Borrado ngx
    Borrado ngx

--- ngx-admin-master/src/model/controlador.ts
+++
@@ -1,9 +0,0 @@
-export class controlador{
-	constructor(
-		public id: string,
-		public invernaderoId: string,
-		public modelo: string,
-		public marca: string,
-		public nroPines: string
-		){}
-}
