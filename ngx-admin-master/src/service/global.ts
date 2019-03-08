7bf29973ec3d89000e00aee203f1dc9780314feb
edsonarios <edsonrios9@gmail.com>
2019-03-07 20:30:44 -0400

    SRC Actualizado
    Actualizacion de SRC

--- ngx-admin-master/src/service/global.ts
+++ ngx-admin-master/src/service/global.ts
@@ -1,6 +1,6 @@
 export var GLOBAL = {
-	url: 'http://18.225.31.160:3000/api/',
-	urlSocket: 'http://18.225.31.160:8080'
+	url: 'http://18.188.248.169:3000/api/',
+	urlSocket: 'http://18.188.248.169:8080'
 	//url: 'http://192.168.1.6:3000/api/',
 	//urlSocket: 'http://192.168.1.6:8080'
 	//url: 'http://localhost:3000/api/',
