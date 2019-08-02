fd4e6b2cfd96a148d01e4c6ea223a26074e5aa2b
evolutionv51 <jorgeluiscastrov5@gmail.com>
2019-08-01 22:58:16 -0400

    botones

--- ngx-admin-master/src/app/usuario/OpAvanzadas/opciones.component.ts
+++ ngx-admin-master/src/app/usuario/OpAvanzadas/opciones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+
 import { Router } from '@angular/router';
 import { DatosNoti } from '../../../model/datosNoti';
 import { GLOBAL } from '../../../service/global';
@@ -8,35 +8,98 @@ import { addSyntheticLeadingComment } from 'typescript';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TrustedHtmlString } from '@angular/core/src/sanitization/bypass';
 
+import { Component, OnDestroy } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+import { takeWhile } from 'rxjs/operators';
+import { SolarData } from '../../@core/data/solar';
+
+interface CardSettings {
+  title: string;
+  iconClass: string;
+  type: string;
+}
 @Component({
   selector: 'ngx-usuario-opAvanzadas',
   styleUrls: ['./opciones.component.scss'],
   templateUrl: './opciones.component.html',
   providers: [OpcionesService],
   animations: [fundido],
-  
-
 })
-export class OpcionesComponent{
-  public Controller;
-  public url: string;
-  contacto: FormGroup;
-  formato: [DatosNoti];
-  formato2: [DatosNoti];
-  public submitted: boolean;
 
-  public idInv=localStorage.getItem('user_inv_id');
-  
+export class OpcionesComponent implements OnDestroy {
+
+  private alive = true;
+  public submitted: boolean;
+  solarValue: number;
+  restartInvCard: CardSettings = {
+    title: 'Reinicio Invernadero',
+    iconClass: 'nb-lightbulb',
+    type: 'primary',
+  };
+  restartPCCard: CardSettings = {
+    title: 'Reinicio PC',
+    iconClass: 'nb-roller-shades',
+    type: 'succes',
+  };
+  backupInvCard: CardSettings = {
+    title: 'Backup Invernadero',
+    iconClass: 'nb-coffe-maker',
+    type: 'warning',
+  };
+
+  statusCards: string;
+
+  commonStatusCardsSet: CardSettings[] = [
+    this.restartInvCard,
+    this.restartPCCard,
+    this.backupInvCard,
+  ];
+
+  statusCardsByThemes: {
+    default: CardSettings[];
+    cosmic: CardSettings[];
+    corporate: CardSettings[];
+    dark: CardSettings[];
+  } = {
+      default: this.commonStatusCardsSet,
+      cosmic: this.commonStatusCardsSet,
+      corporate: [
+        {
+          ...this.restartInvCard,
+          type: 'warning',
+        },
+        {
+          ...this.restartPCCard,
+          type: 'primary',
+        },
+        {
+          ...this.backupInvCard,
+          type: 'danger',
+        },
+      ],
+      dark: this.commonStatusCardsSet,
+    };
 
   constructor(
+    private themeService: NbThemeService,
+    private solarService: SolarData,
     private router: Router,
-    private notiE: OpcionesService,
-    private notiV: OpcionesService,
     private formBuilder: FormBuilder
-  ) { 
+  ) {
     this.submitted = true;
-
+    this.themeService.getJsTheme()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(theme => {
+        this.statusCards = this.statusCardsByThemes[theme.name];
+      });
+    this.solarService.getSolarData()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe((data) => {
+        this.solarValue = data;
+      });
   }
-
+  ngOnDestroy() {
+    this.alive = false;
+  }
 
 }
--- ngx-admin-master/src/app/usuario/OpAvanzadas/opciones.module.ts
+++ ngx-admin-master/src/app/usuario/OpAvanzadas/opciones.module.ts
@@ -8,17 +8,30 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { OpcionesComponent } from './opciones.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbTabsetModule,
+}from '@nebular/theme';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { StatusCardComponent } from './status-card/status-card.component';
+
+
 
 @NgModule({
   imports: [
     ThemeModule,
     NgbModule,
+    NbCardModule,
     FormsModule,
+    NbCardModule,
     ReactiveFormsModule,
     HttpClientModule
   ],
   declarations: [
     OpcionesComponent,
+    StatusCardComponent,
   ],
   providers:[],
   bootstrap:[OpcionesComponent]
